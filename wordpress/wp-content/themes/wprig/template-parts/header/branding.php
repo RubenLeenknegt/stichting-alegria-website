7149318d4f84ee535877b454e96bf6a36812c760
RubenLeenknegt <r.leenknegt@pm.me>
2026-04-16 19:38:48 +0200

    Feat: Made navbar properly accesible

--- wordpress/wp-content/themes/wprig/template-parts/header/branding.php
+++ wordpress/wp-content/themes/wprig/template-parts/header/branding.php
@@ -11,7 +11,8 @@ $logo_svg = wp_rig()->get_logo_svg();
 
 ?>
 <div class="site-branding">
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="site-logo">
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="site-logo" aria-label="Ga naar home pagina">
 		<?php echo $logo_svg; /* phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped */ ?>
 	</a>
 </div>
+
